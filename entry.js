df903459436acf43fb8a2888d4414bb041d88597
Carlos Velasco <carlos@2activedesign.com>
2017-11-16 15:33:42 +0100

    remove mongo.js from entry

--- entry.js
+++ entry.js
@@ -5,5 +5,5 @@ require("./app/css/master.css");
 require("./app/js/offlineRuntimeInstall");
 require("./app/js/master");
 require("./app/js/missions");
-require("./app/js/mongo");
+// require("./app/js/mongo");
 // require("./app/js/geolocation");
