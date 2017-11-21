fae9180481c5e1f83962dc63896c59aa09368209
Carlos Velasco <carlos@2activedesign.com>
2017-11-21 16:08:49 +0100

    prettify some embedded html

--- app/js/missions.js
+++ app/js/missions.js
@@ -217,70 +217,70 @@ trails = new Mission({
     <h3 class="col s12">${this.title}</h3>
     <h5 class="col s12">Georeference</h5>
     <div class="input-field col s6 m3">
-    <input id="Latitude" type="number" value="${window.geoReference.lat}">
-    <label for="Latitude">Latitude</label>
+      <input id="Latitude" type="number" value="${window.geoReference.lat}">
+      <label for="Latitude">Latitude</label>
     </div>
     <div class="input-field col s6 m3">
-    <input id="Longitude" type="number" value="${window.geoReference.long}">
-    <label for="Longitude">Longitude</label>
+      <input id="Longitude" type="number" value="${window.geoReference.long}">
+      <label for="Longitude">Longitude</label>
     </div>
     <div class="input-field col s6 m2">
-    <input id="Altitude" type="number" value="${window.geoReference.alt}">
-    <label for="Altitude">Altitude</label>
+      <input id="Altitude" type="number" value="${window.geoReference.alt}">
+      <label for="Altitude">Altitude</label>
     </div>
     <div class="col s6 m4">
-    <label for="Date">Date</label>
-    <input id="Day" type="text" class="datepicker" value="${new Date().toDateString()}">
+      <label for="Date">Date</label>
+      <input id="Day" type="text" class="datepicker" value="${new Date().toDateString()}">
     </div>
     <h5 class="col s12">Select All that Apply</h5>
-    <div class="col s12 m4">
+    <p class="col s12 m4">
       <label>
         <input id="RootsExposed" type="checkbox">
         <span>Roots Exposed</span>
       </label>
-    </div>
-    <div class="col s12 m4">
+    </p>
+    <p class="col s12 m4">
       <label>
         <input id="Flooded" type="checkbox">
         <span>Flooded</span>
       </label>
-    </div>
-    <div class="col s12 m4">
+    </p>
+    <p class="col s12 m4">
       <label>
         <input id="Bifurcation" type="checkbox">
         <span>Bifurcation - Widening</span>
       </label>
-    </div>
-    <div class="col s12 m4">
+    </p>
+    <p class="col s12 m4">
       <label>
         <input id="FallenTrees" type="checkbox">
         <span>Fallen Trees on Trail</span>
       </label>
-    </div>
-    <div class="col s12 m4">
+    </p>
+    <p class="col s12 m4">
       <label>
         <input id="Slippery" type="checkbox">
         <span>Slippery</span>
       </label>
-    </div>
-    <div class="col s12 m4">
+    </p>
+    <p class="col s12 m4">
       <label>
         <input id="SharpStones" type="checkbox">
         <span>Sharp Stones</span>
       </label>
-    </div>
-    <div class="col s12 m4">
+    </p>
+    <p class="col s12 m4">
       <label>
         <input id="Thorns" type="checkbox">
         <span>Thorny Vegetation on the Edge</span>
       </label>
-    </div>
-    <div class="col s12 m4">
+    </p>
+    <p class="col s12 m4">
       <label>
         <input id="Risk" type="checkbox">
         <span>Risk From Fallen Trees or Branches</span>
       </label>
-    </div>
+    </p>
     <div class="input-field col s12 m4">
       <select id="Erosion">
         <option value="Low">Low</option>
@@ -292,19 +292,18 @@ trails = new Mission({
     <h5 class="col s12">Please Describe</h5>
     <div class="divider"></div>
     <div class="section">
-    <div class="input-field col s12 m6">
-      <textarea id="SupportInfrastructure" class="materialize-textarea"></textarea>
-      <label for="SupportInfrastructure">Support Infrastructure</label>
-      <span class="helper-text">Example: handrails, ropes, steps.</span>
-    </div>
-    <div class="input-field col s12 m6">
-      <textarea id="UsePerception" class="materialize-textarea"></textarea>
-      <label for="UsePerception">
-        Trail Usage</label>
+      <div class="input-field col s12 m6">
+        <textarea id="SupportInfrastructure" class="materialize-textarea"></textarea>
+        <label for="SupportInfrastructure">Support Infrastructure</label>
+        <span class="helper-text">Example: handrails, ropes, steps.</span>
+      </div>
+      <div class="input-field col s12 m6">
+        <textarea id="UsePerception" class="materialize-textarea"></textarea>
+        <label for="UsePerception">
+          Trail Usage</label>
         <span class="helper-text">Example: Many people, conflicts betwen hikers, horses, bicycles.</span>
+      </div>
     </div>
-    </div>
-
     <div class="file-field input-field col s12">
       <div class="btn">
         <span>Photos</span>
@@ -420,34 +419,33 @@ tumlare = new Mission({
       <label for="Longitude">Longitude</label>
     </div>
     <div class="input-field col s6 m4">
-    <label class="" for="Date">Date</label>
+      <label class="" for="Date">Date</label>
       <input id="Day" type="text" class="datepicker" value="${new Date().toDateString()}">
     </div>
     <div class="input-field col s6 m8">
       <label for="Species">Species</label>
       <input id="Species" type="text" value="Porpoise">
     </div>
-    <div class="col s12 m4">
+    <p class="col s12 m4">
       <label>
         <input id="BinocularsUsed" type="checkbox">
         <span>Observation Made with Binoculars</span>
       </label>
-    </div>
+    </p>
     <h5 class="col s12">Quantity</h5>
-
     <div class="col s11 range-field">
-    <label>Quantity</label>
-    <input id="Quantity" type="range" min="1" max="20" value="10">
+      <label>Quantity</label>
+      <input id="Quantity" type="range" min="1" max="20" value="10">
     </div>
     <p class="col s1">
-    <span id="QuantityDisplay" class="helper-text">10</span>
+      <span id="QuantityDisplay" class="helper-text">10</span>
     </p>
-    <div class="col s12">
+    <p class="col s12">
       <label>
         <input id="UncertainQuantity" type="checkbox">
         <span>Uncertain Quantity</span>
       </label>
-    </div>
+    </p>
     <h5 class="col s12">Behavior</h5>
     <div class="input-field col s12 m6">
       <select id="Behavior">
@@ -502,7 +500,6 @@ tumlare = new Mission({
         <input class="file-path validate" type="text" placeholder="Upload one or more photographs of the sighting.">
       </div>
     </div>
-  
     <button class="section col s12 btn btn-large waves-effect waves-light" type="submit" onClick="window.collectInputs('${this
       .databaseCollection}', '${this.congratulatoryMessage}')">Submit
       <i class="material-icons right">send</i>
--- public/js/manifest.js
+++ public/js/manifest.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "./js/" + chunkId + ".js?" + {"0":"81d650807f669d8825b8","1":"70b31f111e4d02a56438"}[chunkId] + "";
+/******/ 		script.src = __webpack_require__.p + "./js/" + chunkId + ".js?" + {"0":"81d650807f669d8825b8","1":"0ebbc19be97bc23a5d12"}[chunkId] + "";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
