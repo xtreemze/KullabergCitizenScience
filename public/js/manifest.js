1e9440edf9f645927bae014c0c45e3b81131c573
Carlos Velasco <carlos@2activedesign.com>
2017-12-01 23:51:11 +0100

    :arrow_up:

--- public/js/manifest.js
+++ public/js/manifest.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "./js/" + chunkId + ".js?" + {"0":"82d1f439189615c571f5","1":"adc721fb69c27ebe7569"}[chunkId] + "";
+/******/ 		script.src = __webpack_require__.p + "./js/" + chunkId + ".js?" + {"0":"2ecca40bbaadc0f5dddd","1":"a27daf963f702dd8d038"}[chunkId] + "";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
