9795d6ae355685a173dccecb4b45ff1216b9518b
S Racheva <gusracsn@student.gu.se>
2017-12-03 14:18:52 +0100

    thing

--- app/js/carousel.js
+++ app/js/carousel.js
@@ -4,13 +4,61 @@
 //init jquery
 
 var elem = document.querySelector('.carousel');
-var instance = new M.Carousel(elem);
+var instance = new M.Carousel(elem, {
+    duration:40
+});
 
-function spin(){
-    instance.next(3);
+function spin() {
+    /*
+     let decision = Math.floor(Math.random()*5);
+     console.log(decision);
+     let delay = 100;
+     console.log(delay);
+     for(let i = 0; i<(decision+delay); i++){
+
+     setTimeout(function () {
+     instance.next();
+
+     },i*100);
+     }*/
+
+    let loops = (Math.floor(Math.random() * 5) + 25);
+    let last = 50;
+
+    for (let i = 0; i < loops; i++) {
+        setTimeout(function () {
+            instance.next();
+        }, i * 100);
+    }
+    ;
+
+
 }
-
 window.onload = spin;
 
+
+const getDelay = (i) =>{
+    if (i > 99){
+        return 300;
+    }
+    if (i > 90){
+        return 200;
+    }
+    else if(i>75){
+        return 150;
+    }
+    else if(i>50){
+        return 100;
+    }
+    else{
+        return 70;
+    }
+};
 //decide on prize
-//
+//0 = bottle
+//1 = coffee
+//2 = fika
+//3 = tour
+//4 = towel
+
+
